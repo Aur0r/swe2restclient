2e87017e4d59b8f6b7908ca561be78ed92201f5e
Martin <Martin@Martin-Envy.fritz.box>
2011-01-22 20:06:43 +0100

    Artikelsuche hinzugefügt.

--- includes/php/views/MainView.php
+++ includes/php/views/MainView.php
@@ -3,5 +3,6 @@
 <div id="content">
 	<ul id="menu">
 		<li id="menuitem-person"><a href="<?php print $FILE_URL; ?>?bereich=profilsuche">Profilsuche</a></li>
+		<li id="menuitem-artikel"><a href="<?php print $FILE_URL; ?>?bereich=artikelsuche">Artikelsuche</a></li>
 	</ul>
 </div>
